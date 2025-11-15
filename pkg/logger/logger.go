f6770ebf30a577d0db1af384d56a195754df768b
k.rogachev <kirya.rog@ya.ru>
2025-11-15 18:36:59 +0300

    feat: logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,38 @@
 package logger
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+)
+
+func New(level string) (*zap.Logger, error) {
+	var cfg zap.Config
+
+	switch level {
+	case "debug":
+		cfg = zap.NewDevelopmentConfig()
+		cfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	case "info":
+		cfg = zap.NewProductionConfig()
+		cfg.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+
+	case "warn":
+		cfg = zap.NewProductionConfig()
+		cfg.Level = zap.NewAtomicLevelAt(zap.WarnLevel)
+
+	case "error":
+		cfg = zap.NewProductionConfig()
+		cfg.Level = zap.NewAtomicLevelAt(zap.ErrorLevel)
+	default:
+		return nil, fmt.Errorf("invalid log level: %s", level)
+	}
+
+	cfg.DisableStacktrace = true
+
+	logger, err := cfg.Build()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create logger: %w", err)
+	}
+
+	return logger, nil
+}
